e15aa7ef8c88a46a719203e89d8ef079b44d8b41
hariirawan <hari31121993@gmail.com>
2025-03-22 02:15:16 +0800

    feat(security): adding api for cookies with security

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("token")?.value;
+  const { pathname } = req.nextUrl;
+
+  // Daftar halaman yang perlu authentication
+  const isProtectedRoute = pathname.startsWith("/admin");
+  const isLoginPage = pathname.startsWith("/login");
+  const isHomePage = pathname === "/";
+
+  if ((isProtectedRoute || isHomePage) && !token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  // Jika sudah login tapi mengakses /login, redirect ke /admin
+  if (isLoginPage && token) {
+    return NextResponse.redirect(new URL("/admin", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/admin/:path*", "/login"],
+};

07bd5f7c2697f302d91ef011fc3f327cc94913cd
hariirawan <hari31121993@gmail.com>
2025-03-22 04:10:09 +0800

    chore!: adding config for pm2 to run in vps

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "maujutest.hariirawan.my.id",
+      script: "bun",
+      args: "start",
+      cwd: "/var/www/html/gofinance",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3008,
+      },
+    },
+  ],
+};
